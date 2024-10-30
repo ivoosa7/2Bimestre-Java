8e0ad08cf2a6b064e73dcb5e66bed1ce19616f41
Ivo Araujo <ivo.souza-23@outlook.com>
2024-10-30 09:00:10 -0300

    Make Id like primary key and actions HashCode and Override

--- product/src/main/java/br/araujo/product/entities/Product.java
+++ product/src/main/java/br/araujo/product/entities/Product.java
@@ -1,16 +1,20 @@
 package br.araujo.product.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Product {
     
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private double price;
     private String category;
+    
     public long getId() {
         return id;
     }
