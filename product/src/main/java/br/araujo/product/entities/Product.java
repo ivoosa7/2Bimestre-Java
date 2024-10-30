864d9ac57ad285f10d014495e005aa875d3447bd
Ivo Araujo <ivo.souza-23@outlook.com>
2024-10-30 00:30:28 -0300

    Entities folder was cretated

---
+++ product/src/main/java/br/araujo/product/entities/Product.java
@@ -0,0 +1,35 @@
+package br.araujo.product.entities;
+
+public class Product {
+    
+    private long id;
+    private String name;
+    private double price;
+    private String category;
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    
+}
