fef53eb8e2d5648c449a7bf07d3b6ed835e0a4cf
Ivo Araujo <ivo.souza-23@outlook.com>
2024-10-30 00:15:34 -0300

    Java Spring Boot Initial

---
+++ product/src/test/java/br/araujo/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package br.araujo.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
